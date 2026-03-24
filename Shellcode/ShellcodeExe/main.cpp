6db5bd73901c02755180277f849822ad18bf6fcd
Vigo0x1 <109324020+vuongle-vigo@users.noreply.github.com>
2026-03-24 09:50:44 +0700

    Update shell

--- Shellcode/ShellcodeExe/main.cpp
+++ Shellcode/ShellcodeExe/main.cpp
@@ -1,14 +1,6 @@
 #include <Windows.h>
 
-void test() {
-	while (1){
-		int a = 1;
-		int b = 2;
-		int c = a + b;
-	}
-}
 int main() {
 	char dataString[] = { 'H', 'e', 'l', 'l', 'o', 'W', 'o', 'u', 'l', 'd'};
-	MessageBoxA(NULL, NULL, NULL, MB_OK);
-	CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)test, NULL, 0, NULL);
+	char* dataString2 = "HelloWorld";
 }
